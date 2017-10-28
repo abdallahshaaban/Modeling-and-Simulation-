e01cfa412245ebd4a913d674a1d38f7a975f9484
Abdallah Shaaban <abdallah.shaaban@cis.asu.edu.eg>
2017-10-28 21:33:05 +0200

    table generated

---
+++ Task2/newspapersellersimulation_students/newspapersellersimulation/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace NewspaperSellerSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Demand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Good = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fair = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Poor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.ProbabilityOfGoodDay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProbabilityOfFairDay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProbabilityOfPoorDay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNumOfNewpapers = new System.Windows.Forms.TextBox();
+            this.txtNumOfDays = new System.Windows.Forms.TextBox();
+            this.txtPurchasePrice = new System.Windows.Forms.TextBox();
+            this.txtSellingPrice = new System.Windows.Forms.TextBox();
+            this.txtScrapPrice = new System.Windows.Forms.TextBox();
+            this.btnReadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Demand,
+            this.Good,
+            this.Fair,
+            this.Poor});
+            this.dataGridView1.Location = new System.Drawing.Point(942, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(458, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Demand
+            // 
+            this.Demand.HeaderText = "Demand";
+            this.Demand.Name = "Demand";
+            // 
+            // Good
+            // 
+            this.Good.HeaderText = "Good";
+            this.Good.Name = "Good";
+            // 
+            // Fair
+            // 
+            this.Fair.HeaderText = "Fair";
+            this.Fair.Name = "Fair";
+            // 
+            // Poor
+            // 
+            this.Poor.HeaderText = "Poor";
+            this.Poor.Name = "Poor";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProbabilityOfGoodDay,
+            this.ProbabilityOfFairDay,
+            this.ProbabilityOfPoorDay});
+            this.dataGridView2.Location = new System.Drawing.Point(942, 470);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(450, 80);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // ProbabilityOfGoodDay
+            // 
+            this.ProbabilityOfGoodDay.HeaderText = "Probability of Good Day";
+            this.ProbabilityOfGoodDay.Name = "ProbabilityOfGoodDay";
+            // 
+            // ProbabilityOfFairDay
+            // 
+            this.ProbabilityOfFairDay.HeaderText = "Probability Of Fair Day";
+            this.ProbabilityOfFairDay.Name = "ProbabilityOfFairDay";
+            // 
+            // ProbabilityOfPoorDay
+            // 
+            this.ProbabilityOfPoorDay.HeaderText = "Probability Of Poor Day";
+            this.ProbabilityOfPoorDay.Name = "ProbabilityOfPoorDay";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(54, 110);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(754, 377);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(914, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Num Of News papers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(914, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Num Of Days";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1157, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Purchase Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1157, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Selling Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1157, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Scrap Price";
+            // 
+            // txtNumOfNewpapers
+            // 
+            this.txtNumOfNewpapers.Location = new System.Drawing.Point(1041, 9);
+            this.txtNumOfNewpapers.Name = "txtNumOfNewpapers";
+            this.txtNumOfNewpapers.Size = new System.Drawing.Size(100, 20);
+            this.txtNumOfNewpapers.TabIndex = 8;
+            // 
+            // txtNumOfDays
+            // 
+            this.txtNumOfDays.Location = new System.Drawing.Point(1041, 42);
+            this.txtNumOfDays.Name = "txtNumOfDays";
+            this.txtNumOfDays.Size = new System.Drawing.Size(100, 20);
+            this.txtNumOfDays.TabIndex = 9;
+            // 
+            // txtPurchasePrice
+            // 
+            this.txtPurchasePrice.Location = new System.Drawing.Point(1252, 9);
+            this.txtPurchasePrice.Name = "txtPurchasePrice";
+            this.txtPurchasePrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPurchasePrice.TabIndex = 10;
+            // 
+            // txtSellingPrice
+            // 
+            this.txtSellingPrice.Location = new System.Drawing.Point(1252, 46);
+            this.txtSellingPrice.Name = "txtSellingPrice";
+            this.txtSellingPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtSellingPrice.TabIndex = 11;
+            // 
+            // txtScrapPrice
+            // 
+            this.txtScrapPrice.Location = new System.Drawing.Point(1252, 91);
+            this.txtScrapPrice.Name = "txtScrapPrice";
+            this.txtScrapPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtScrapPrice.TabIndex = 12;
+            // 
+            // btnReadData
+            // 
+            this.btnReadData.Location = new System.Drawing.Point(993, 576);
+            this.btnReadData.Name = "btnReadData";
+            this.btnReadData.Size = new System.Drawing.Size(75, 23);
+            this.btnReadData.TabIndex = 13;
+            this.btnReadData.Text = "Read data";
+            this.btnReadData.UseVisualStyleBackColor = true;
+            this.btnReadData.Click += new System.EventHandler(this.btnReadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1446, 697);
+            this.Controls.Add(this.btnReadData);
+            this.Controls.Add(this.txtScrapPrice);
+            this.Controls.Add(this.txtSellingPrice);
+            this.Controls.Add(this.txtPurchasePrice);
+            this.Controls.Add(this.txtNumOfDays);
+            this.Controls.Add(this.txtNumOfNewpapers);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Demand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Good;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fair;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Poor;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProbabilityOfGoodDay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProbabilityOfFairDay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProbabilityOfPoorDay;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNumOfNewpapers;
+        private System.Windows.Forms.TextBox txtNumOfDays;
+        private System.Windows.Forms.TextBox txtPurchasePrice;
+        private System.Windows.Forms.TextBox txtSellingPrice;
+        private System.Windows.Forms.TextBox txtScrapPrice;
+        private System.Windows.Forms.Button btnReadData;
+    }
+}
---
+++ Task2/newspapersellersimulation_students/newspapersellersimulation/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewspaperSellerTesting;
+using NewspaperSellerModels;
+
+namespace NewspaperSellerSimulation
+{
+    public partial class Form1 : Form
+    {
+        public List<DayTypeDistribution> dayTypeDis;
+        public List<DemandDistribution> demandDis;
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dayTypeDis = new List<DayTypeDistribution>();
+            demandDis = new List<DemandDistribution>();
+        }
+        private void Read_Demand_Distribution()
+        {
+            int m = dataGridView1.Rows.Count;
+            double cumlativeGoodDay = 0;
+            double cumlativeFairDay = 0;
+            double cumlativePoorDay = 0;
+
+            for (int i = 0; i < m - 1; ++i) {
+                cumlativeGoodDay += Math.Round(float.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString()), 5);
+                cumlativeFairDay += Math.Round(float.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString()), 5);
+                cumlativePoorDay += Math.Round(float.Parse(dataGridView1.Rows[i].Cells[3].Value.ToString()), 5);
+
+
+            }
+            if (cumlativeFairDay == 1 && cumlativeGoodDay == 1 && cumlativePoorDay == 1)
+            {
+                
+                for (int i = 0; i < m - 1; ++i)
+                {
+
+                    demandDis.Add(new DemandDistribution());
+                    demandDis[i].Demand = Int32.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    double test = demandDis[i].Demand % 10;
+
+                    if (test != 0.0)
+                    {
+                        string error = "demand # : " + i + "is not multiple of 10";
+                        MessageBox.Show(error);
+                        demandDis.Clear();
+                    }
+                    else
+                    {
+
+                        demandDis[i].DayTypeDistributions.Add(new DayTypeDistribution());
+                        demandDis[i].DayTypeDistributions[0].Probability = Math.Round(float.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString()), 5);
+                        demandDis[i].DayTypeDistributions[0].DayType = Enums.DayType.Good;
+                        demandDis[i].DayTypeDistributions.Add(new DayTypeDistribution());
+
+                        demandDis[i].DayTypeDistributions[1].Probability = Math.Round(float.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString()), 5);
+                        demandDis[i].DayTypeDistributions[1].DayType = Enums.DayType.Fair;
+
+                        demandDis[i].DayTypeDistributions.Add(new DayTypeDistribution());
+                        demandDis[i].DayTypeDistributions[2].Probability = Math.Round(float.Parse(dataGridView1.Rows[i].Cells[3].Value.ToString()), 5);
+                        demandDis[i].DayTypeDistributions[2].DayType = Enums.DayType.Poor;
+
+                    }
+
+                }
+
+
+
+            }
+            else { MessageBox.Show("cumulative probability is NOT equal 1"); }
+        }
+
+
+        private void Read_Day_Type_Distribution()
+        {
+            int m = dataGridView2.Rows.Count;
+            if (m != 2)
+            {
+                MessageBox.Show("Day Type Distribution must be one Row");
+            }
+            else {
+                double test = 0;
+                test += Math.Round(float.Parse(dataGridView2.Rows[0].Cells[0].Value.ToString()), 5);
+                test += Math.Round(float.Parse(dataGridView2.Rows[0].Cells[1].Value.ToString()), 5);
+                test += Math.Round(float.Parse(dataGridView2.Rows[0].Cells[2].Value.ToString()), 5);
+
+
+                if (test == 1)
+                {
+                    dayTypeDis.Add(new DayTypeDistribution());
+                    dayTypeDis[0].Probability = Math.Round(float.Parse(dataGridView2.Rows[0].Cells[0].Value.ToString()), 5);
+                    dayTypeDis[0].DayType = Enums.DayType.Good;
+                    dayTypeDis.Add(new DayTypeDistribution());
+                    dayTypeDis[1].Probability = Math.Round(float.Parse(dataGridView2.Rows[0].Cells[1].Value.ToString()), 5);
+                    dayTypeDis[1].DayType = Enums.DayType.Fair;
+                    dayTypeDis.Add(new DayTypeDistribution());
+                    dayTypeDis[2].Probability = Math.Round(float.Parse(dataGridView2.Rows[0].Cells[2].Value.ToString()), 5);
+                    dayTypeDis[2].DayType = Enums.DayType.Poor;
+                }
+                else { MessageBox.Show("cumulative probability is NOT equal 1"); }
+            }
+        }
+        private void btnReadData_Click(object sender, EventArgs e)
+        {
+            NewspaperSellerModels.System sys = new NewspaperSellerModels.System();
+
+            dayTypeDis = new List<DayTypeDistribution>();
+            demandDis = new List<DemandDistribution>();
+            //string test = TestingManager.Test(sys, Constants.FileNames.TestCase1);
+            //MessageBox.Show(test);
+
+            Read_Demand_Distribution();
+            Read_Day_Type_Distribution();
+            Helper.Calc_Ranges(dayTypeDis);
+            Helper.Calc_Ranges_Of_Demand(demandDis);
+
+            //test
+            /*for (int i = 0; i < demandDis.Count; i++) {
+                  MessageBox.Show(demandDis[i].DayTypeDistributions[0].MinRange.ToString());
+                  MessageBox.Show(demandDis[i].DayTypeDistributions[0].MaxRange.ToString());
+                  MessageBox.Show(demandDis[i].DayTypeDistributions[1].MinRange.ToString());
+                  MessageBox.Show(demandDis[i].DayTypeDistributions[1].MaxRange.ToString());
+                  MessageBox.Show(demandDis[i].DayTypeDistributions[2].MinRange.ToString());
+                  MessageBox.Show(demandDis[i].DayTypeDistributions[2].MaxRange.ToString());
+              }
+
+            for (int i = 0; i < dayTypeDis.Count; i++)
+            {
+                MessageBox.Show(dayTypeDis[i].CummProbability.ToString());
+                MessageBox.Show(dayTypeDis[i].MinRange.ToString());
+                MessageBox.Show(dayTypeDis[i].MaxRange.ToString());
+                
+            }
+            */
+            sys.NumOfNewspapers = Int32.Parse( txtNumOfNewpapers.Text.ToString());
+            sys.NumOfRecords = Int32.Parse(txtNumOfDays.Text.ToString());
+            sys.PurchasePrice= Math.Round(float.Parse(txtPurchasePrice.Text.ToString()),5);
+            sys.ScrapPrice = Math.Round(float.Parse(txtScrapPrice.Text.ToString()),5);
+            sys.SellingPrice = Math.Round(float.Parse(txtSellingPrice.Text.ToString()),5);
+            sys.DayTypeDistributions = dayTypeDis;
+            sys.DemandDistributions = demandDis;
+            sys.UnitProfit = sys.PurchasePrice - sys.SellingPrice;
+            List<SimulationCase> simulation = Helper.simulate(sys);
+            dataGridView3.DataSource = simulation;
+           
+        }
+    }
+}
